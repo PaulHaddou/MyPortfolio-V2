7fae865aaf074c173af313a89a1fb4b9ae207466
Paul Haddou <paul2405.63@hotmail.fr>
2020-07-22 16:10:32 +0200

    v1.5.4 :sparkles: Added new projects on the home page. :camera_flash: Added new images associated with new projects. :fire: Removed Accio and Paul-Haddou projects. :building_construction: Added a reference file for all the texts on the site.

---
+++ includes/texts/texts-FR.php
@@ -0,0 +1,73 @@
+<?php
+
+$project_Position_Webmaster = 'Webmaster';
+$project_Position_Motion = 'Motion Designer';
+$project_Position_Front = 'Dev Front-End';
+$project_Position_UI = 'UI Designer';
+$project_Position_Graphic = 'Graphiste';
+$project_Position_Writer = 'Redacteur';
+$project_Position_Swift = 'Dev Swift';
+$project_Position_UX = 'UX Designer';
+
+$project_Studio_Haddou_Dufourcq_Name = 'Studio Haddou / Dufourcq';
+$project_Studio_Haddou_Dufourcq_Thumbnail = 'images/thumbnails/Paul-Haddou_Thumbnail-Projects_Studio-HD.jpg';
+$project_Studio_Haddou_Dufourcq_Link = 'pages/projets/Studio-Haddou-Dufourcq/';
+
+$project_Affiches_Synerg_Name = 'Affiches de campagne Synerg\'hetic';
+$project_Affiches_Synerg_Thumbnail = 'images/thumbnails/Paul-Haddou_Thumbnail-Projects_Affiches-Synerg.jpg';
+$project_Affiches_Synerg_Link = 'pages/projets/Affiches-Synerg/';
+
+$project_Animation_JS_Name = 'Animation JS';
+$project_Animation_JS_Thumbnail = 'images/thumbnails/Paul-Haddou_Thumbnail-Projects_Animation-JS.jpg';
+$project_Animation_JS_Link = 'pages/projets/Animation-JS/';
+
+$project_Barathon_Name = 'Barathon';
+$project_Barathon_Thumbnail = 'images/thumbnails/Paul-Haddou_Thumbnail-Projects_Barathon.jpg';
+$project_Barathon_Link = 'pages/projets/Barathon/';
+
+$project_Affiche_Deadpool_Name = 'Affiche Deadpool 2';
+$project_Affiche_Deadpool_Thumbnail = 'images/thumbnails/Paul-Haddou_Thumbnail-Projects_Affiche-Deadpool.jpg';
+$project_Affiche_Deadpool_Link = 'pages/projets/Affiche-Deadpool-2/';
+
+$project_Matt_Painting_Name = 'Matt Painting';
+$project_Matt_Painting_Thumbnail = 'images/thumbnails/Paul-Haddou_Thumbnail-Projects_Matt-Painting.jpg';
+$project_Matt_Painting_Link = 'pages/projets/Matt-Painting/';
+
+$project_Exercice_Mise_en_Page_Name = 'Mise en page';
+$project_Exercice_Mise_en_Page_Thumbnail = 'images/thumbnails/Paul-Haddou_Thumbnail-Projects_Mise-en-Page-exercice.jpg';
+$project_Exercice_Mise_en_Page_Link = 'pages/projets/Mise-en-Page-Exercice/';
+
+$project_My_Pills_Name = 'My Pills';
+$project_My_Pills_Thumbnail = 'images/thumbnails/Paul-Haddou_Thumbnail-Projects_My-Pills.jpg';
+$project_My_Pills_Link = 'pages/projets/My-Pills/';
+
+$project_Newsletter_Refonte_Name = 'Newsletter Cuure';
+$project_Newsletter_Refonte_Thumbnail = 'images/thumbnails/Paul-Haddou_Thumbnail-Projects_Newsletter-Refonte.jpg';
+$project_Newsletter_Refonte_Link = 'pages/projets/Newsletter-Refonte/';
+
+$project_Newsletter_BPCE_Name = 'Newsletter BPCE';
+$project_Newsletter_BPCE_Thumbnail = 'images/thumbnails/Paul-Haddou_Thumbnail-Projects_Newsletter-BPCE.jpg';
+$project_Newsletter_BPCE_Link = 'pages/projets/Newsletter-BPCE/';
+
+$project_Motion_Animation_Logo_Name = 'Motion animation de logo';
+$project_Motion_Animation_Logo_Thumbnail = 'images/thumbnails/Paul-Haddou_Thumbnail-Projects_Motion-Animation-de-Logo.mp4';
+$project_Motion_Animation_Logo_Link = 'pages/projets/Motion-Animation-Logo/';
+
+
+
+$projectsArray = array (
+
+    //array("titre","poste","image","link"),
+
+    array("$project_Studio_Haddou_Dufourcq_Name","$project_Position_Webmaster - $project_Position_Front","$project_Studio_Haddou_Dufourcq_Thumbnail","$project_Studio_Haddou_Dufourcq_Link"),
+    array("$project_Motion_Animation_Logo_Name","$project_Position_Motion","$project_Motion_Animation_Logo_Thumbnail","$project_Motion_Animation_Logo_Link"),    
+    array("$project_Newsletter_Refonte_Name","$project_Position_UI","$project_Newsletter_Refonte_Thumbnail","$project_Newsletter_Refonte_Link"),    
+    array("$project_My_Pills_Name","$project_Position_UX - $project_Position_UI","$project_My_Pills_Thumbnail","$project_My_Pills_Link"),    
+    array("$project_Newsletter_BPCE_Name","$project_Position_UI - $project_Position_Writer","$project_Newsletter_BPCE_Thumbnail","$project_Newsletter_BPCE_Link"),    
+    array("$project_Affiches_Synerg_Name","$project_Position_Graphic","$project_Affiches_Synerg_Thumbnail","$project_Affiches_Synerg_Link"),
+    array("$project_Animation_JS_Name","$project_Position_Front","$project_Animation_JS_Thumbnail","$project_Animation_JS_Link"),    
+    array("$project_Exercice_Mise_en_Page_Name","$project_Position_UI","$project_Exercice_Mise_en_Page_Thumbnail","$project_Exercice_Mise_en_Page_Link"),    
+    array("$project_Barathon_Name","$project_Position_Swift","$project_Barathon_Thumbnail","$project_Barathon_Link"),
+    array("$project_Matt_Painting_Name","$project_Position_Graphic","$project_Matt_Painting_Thumbnail","$project_Matt_Painting_Link"),
+    array("$project_Affiche_Deadpool_Name","$project_Position_Graphic","$project_Affiche_Deadpool_Thumbnail","$project_Affiche_Deadpool_Link"),
+    );
--- index.php
+++ index.php
@@ -27,38 +27,103 @@
         $spanProjects = '<span class="spanActiv" ></span>';
         
         include 'includes/navbar.php';
-        include 'includes/navbarMobile.php'
-
+        include 'includes/navbarMobile.php';
+        include 'includes/texts/texts-FR.php';
+        
     ?>
     <div class="main">
-        <div class="main_projects">
-        <div class="projects_project">
-                <a href="pages/projets/mon-intro/">
-                    <video playsinline autoplay muted loop src="images/Intro_Paul-Haddou.mp4" alt="Motion de mon intro"></video>
-                </a>
-                <div class="project_description">
-                    <p>Mon Intro</p>
-                    <p>Motion Design</p>
+        <div class="main_projects_1">
+        <?php
+            $length = count($projectsArray);
+            for ($i = 0; $i < 1; $i++) 
+            {
+            ?>
+                <div class="projects_project">
+                    <a href="<?= $projectsArray[$i][3] ?>">
+                        <img src="<?= $projectsArray[$i][2] ?>" alt="fff">
+                    </a>
+                    <div class="project_description">
+                        <p><?= $projectsArray[$i][0] ?></p>
+                        <p><?= $projectsArray[$i][1] ?></p>
+                    </div>
                 </div>
-            </div>
+            <?php
+            }
+            for ($i = 1; $i < 2; $i++) 
+            {
+            ?>
+                <div class="projects_project">
+                    <a href="<?= $projectsArray[$i][3] ?>">
+                        <video playsinline autoplay muted loop src="<?= $projectsArray[$i][2] ?>" alt="Motion de mon intro"></video>
+                    </a>
+                    <div class="project_description">
+                        <p><?= $projectsArray[$i][0] ?></p>
+                        <p><?= $projectsArray[$i][1] ?></p>
+                    </div>
+                </div>
+            <?php
+            }
+            for ($i = 2; $i < 4; $i++) 
+            {
+            ?>
+                <div class="projects_project">
+                    <a href="<?= $projectsArray[$i][3] ?>">
+                        <img src="<?= $projectsArray[$i][2] ?>" alt="fff">
+                    </a>
+                    <div class="project_description">
+                        <p><?= $projectsArray[$i][0] ?></p>
+                        <p><?= $projectsArray[$i][1] ?></p>
+                    </div>
+                </div>
+            <?php
+            }
+        ?>
+        </div>
+        <div class="main_projects_2">
+        <?php
+            $length = count($projectsArray);
+            for ($i = 4; $i < 8; $i++) 
+            {
+        ?>
             <div class="projects_project">
-                <a href="pages/projets/haddou-dufourcq/">
-                    <img src="images/haddou-dufourcq.jpg" alt="Image du projet de site vitrine haddou-dufourcq.com">
+                <a href="<?= $projectsArray[$i][3] ?>">
+                    <img src="<?= $projectsArray[$i][2] ?>" alt="fff">
                 </a>
                 <div class="project_description">
-                    <p>Site Haddou-Dufourcq</p>
-                    <p>Front-End - Webmaster</p>
+                    <p><?= $projectsArray[$i][0] ?></p>
+                    <p><?= $projectsArray[$i][1] ?></p>
                 </div>
             </div>
+        <?php
+            }
+        ?>
+        </div>
+        <div class="main_projects_3">
+        <?php
+            $length = count($projectsArray);
+            for ($i = 8; $i < $length; $i++) 
+            {
+        ?>
             <div class="projects_project">
-                <a href="pages/projets/paul-haddou/">
-                    <img src="images/paul-haddou.jpg" alt="Photo du projet de mon portfolio">
+                <a href="<?= $projectsArray[$i][3] ?>">
+                    <img src="<?= $projectsArray[$i][2] ?>" alt="fff">
                 </a>
                 <div class="project_description">
-                    <p>Mon Portfolio</p>
-                    <p>Design UI/UX - Front-End</p>
+                    <p><?= $projectsArray[$i][0] ?></p>
+                    <p><?= $projectsArray[$i][1] ?></p>
                 </div>
             </div>
+        <?php
+            }
+        ?>
+            <div class="projects_project more-soon">
+                <div class="more-soon_text">
+                    <p>+ Bientôt</p>
+                </div>
+            </div>
+        </div>
+        <div class="projects_button">
+            <a class="projects_button_download" href="pdf/CV_Paul-Haddou.pdf" download>Mon portfolio au format PDF</a>
         </div>
     </div>
     <?php include 'includes/footer.php' ?>
@@ -66,3 +131,4 @@
     <script src="sources/burger.js"></script>
 </body>
 </html>
+<!---                    <video playsinline autoplay muted loop src="<?= $projectsArray[$i][2] ?>" alt="Motion de mon intro"></video> --->
